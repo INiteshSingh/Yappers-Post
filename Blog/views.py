5ed6fdfaca36332960053457b0398fea4f53e07d
Thakur Nitesh Singh <166356896+INiteshSingh@users.noreply.github.com>
2025-09-29 16:08:07 +0530

    Made the Basic Outline For the Project

--- Blog/views.py
+++ Blog/views.py
@@ -7,13 +7,15 @@ def home(request):
 
 def blog_form(request):
     form = write_your_blog()
+    submit_status = False
     if request.method == "POST":
         form = write_your_blog(request.POST, request.FILES)
+        submit_status = True
         if form.is_valid():
             form.save()
             print("Blog Saved")    
             
-    return render(request,"Blog/blogform.html",{"form":form})
+    return render(request,"Blog/blogform.html",{"form":form,"submit_status":submit_status})
 
 def view_blogs(request):
     current_blogs = Blog_Data.objects.all()
