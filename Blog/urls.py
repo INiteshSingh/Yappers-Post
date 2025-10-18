1a39e496cab4353986ce913a70ff1d85bcbe1f0c
Thakur Nitesh Singh <166356896+INiteshSingh@users.noreply.github.com>
2025-10-18 22:25:18 +0530

    Added Some Paths
    Added the home path in urls.py

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home',views.home,name="home"),
+    path('',views.home,name="home"),
     path('blog_form',views.blog_form,name="blog_form"),
     path('view_blogs',views.view_blogs,name="view_blogs")
 ]
